e02d9e71463a53a567b97f1bbe80feee0c2e2490
shimonuri <54213090+shimonuri@users.noreply.github.com>
2022-12-03 18:26:36 +0200

    adding files

---
+++ collisions.py
@@ -0,0 +1,20 @@
+def find_dtwall(ball):
+    # Find the distance to the wall
+    # with the ultrasonic sensor
+    # and return the distance
+    return 0.0
+
+
+def find_dtcol(ball_1, ball_2):
+    # Find the distance to the collision
+    # with the infrared sensor
+    # and return the distance
+    return 0.0
+
+
+def make_collision(ball_1, ball_2):
+    # Make the collision
+    # between the two balls
+    # and return the new velocity
+    # of the two balls
+    return 0.0, 0.0
---
+++ model.py
@@ -0,0 +1,9 @@
+import dataclasses
+
+@dataclasses.dataclass
+class Ball:
+    x: float
+    y: float
+    vx: float
+    vy: float
+    radius: float
