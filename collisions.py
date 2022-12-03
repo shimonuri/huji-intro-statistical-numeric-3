99d2e21929819e815534e5f61ecb175e924007ac
AR1905 <assaf.reiner@gmail.com>
2022-12-03 19:01:19 +0200

    Update collisions.py
    find_dtcoll function

--- collisions.py
+++ collisions.py
@@ -1,17 +1,18 @@
 import numpy as np
+from numpy.linalg import norm
 
 def find_dtwall(ball):
     x, y, vx, vy, r = ball.x, ball.y, ball.vx, ball.vy, ball.radius
     if vx > 0:
         dtwall_x = (1 - r - x) / vx
-    elif vx < 0
+    elif vx < 0:
         dtwall_x = (x - r) / abs(vx)
     else:
         dtwall_x = 10 ** 10 # Practically infinity
 
     if vy > 0:
         dtwall_y = (1 - r - y) / vy
-    elif vx < 0
+    elif vx < 0:
         dtwall_y = (y - r) / abs(vy)
     else:
         dtwall_y = 10 ** 10 # Practically infinity
@@ -20,6 +21,8 @@ def find_dtwall(ball):
 
 
 def find_dtcoll(ball_1, ball_2):
+    r = ball_1.radius
+
     delta_x = ball_2.x - ball_1.x
     delta_y = ball_2.y - ball_1.y
     delta_l = np.array([delta_x, delta_y])
@@ -29,6 +32,16 @@ def find_dtcoll(ball_1, ball_2):
     delta_v = np.array(delta_vx, delta_vy)
 
     s = np.dot(delta_v, delta_l)
+
+    det = s ** 2 - (norm(delta_v) ** 2) * (norm(delta_l) ** 2 - 4 * r ** 2)
+
+    if det > 0 and s< 0:
+        dtcoll = - (s + np.sqrt(det)) / (norm(delta_v) ** 2)
+    else:
+        dtcoll = 10 ** 10 # Practically infinity
+
+    return dtcoll
+
 
 def make_wall_collision(ball):
     return 0.0, 0.0
